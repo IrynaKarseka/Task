d84468f5ec3c4160628a41b367d6463cd0620586
Iryna Karseka <Karsira88@mail.ru>
2023-05-20 12:33:47 +0300

    Added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factNumbers = FactNumbers(number);
+Console.WriteLine($"Умножение чисел от 1 до {number} = {factNumbers}");
+
+int FactNumbers(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
