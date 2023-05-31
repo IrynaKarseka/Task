8fcbf31e017b8559909b5e5a9430c7bddbe5e61a
Iryna Karseka <Karsira88@mail.ru>
2023-05-31 10:42:15 +0300

    Added Task41/

---
+++ Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. 
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int CountNumGreaterZero(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count += 1;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите длинну массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size);
+
+PrintArray(array, ",");
+Console.Write(" -> ");
+
+int counterPositiv = CountNumGreaterZero(array);
+Console.Write(counterPositiv);
