ba78f03ba473d7e8618a86a191feebe984a4659a
Iryna Karseka <Karsira88@mail.ru>
2023-06-06 15:47:04 +0300

    Added Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumDigitsInNumberRec = SumDigitsInNumberRec(number);
+Console.WriteLine($"Cумма цифр в числе {number} равна {sumDigitsInNumberRec}");
+
+int SumDigitsInNumberRec(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + SumDigitsInNumberRec(n / 10);
+}
